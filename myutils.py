d46a69d1e39894f1da8cdb06752bbea34d61d51b
Julius de Blaaij <julius.de.blaaij@gmail.com>
2023-05-24 22:14:03 +0200

    Added xDNN classify, eval_xDNN_DoX, extended myutils to have stemming

--- myutils.py
+++ myutils.py
@@ -1,5 +1,7 @@
 import re
+from nltk.stem import PorterStemmer
 
+ps = PorterStemmer()
 
 def pre_process_text(text):
     text = re.sub('[^a-zA-Z0-9 -]', '', str(text))
@@ -7,3 +9,12 @@ def pre_process_text(text):
     text = text.lower()
 
     return text
+
+def stem_text(text):
+    words = text.split(' ')
+    stemmed_text = ""
+
+    for word in words:
+        stemmed_text += " " + ps.stem(word)
+
+    return stemmed_text.lstrip(' ')
--- xDNN/xDNN_run_custom.py
+++ xDNN/xDNN_run_custom.py
@@ -9,7 +9,7 @@ Angelov, P., & Soares, E. (2020). Towards explainable deep neural networks (xDNN
 ###############################################################################
 import pandas as pd
 
-from xDNN_class import *
+from xDNN.xDNN_class import *
 from numpy import genfromtxt
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
@@ -21,28 +21,28 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 import time
+from doc2vec import doc2vec
+import doxpy
+# Load the files, including features, images and labels.
 
-# Load the files, including features, images and labels. 
-
-X_train_file_path = r'../data_df_X_train.csv'
-y_train_file_path = r'../data_df_y_train.csv'
-X_test_file_path = r'../data_df_X_test.csv'
-y_test_file_path = r'../data_df_y_test.csv'
+X_train_file_path = r'C:/Users/SKIKK/PycharmProjects/XAI/data_df_X_train.csv'
+y_train_file_path = r'C:/Users/SKIKK/PycharmProjects/XAI/data_df_y_train.csv'
+X_test_file_path = r'C:/Users/SKIKK/PycharmProjects/XAI/data_df_X_test.csv'
+y_test_file_path = r'C:/Users/SKIKK/PycharmProjects/XAI/data_df_y_test.csv'
 
 X_train = genfromtxt(X_train_file_path, delimiter=',')
-y_train = pd.read_csv(y_train_file_path, delimiter=',',header=None)
+y_train = pd.read_csv(y_train_file_path, delimiter=',', header=None)
 X_test = genfromtxt(X_test_file_path, delimiter=',')
-y_test = pd.read_csv(y_test_file_path, delimiter=',',header=None)
+y_test = pd.read_csv(y_test_file_path, delimiter=',', header=None)
 
 # Print the shape of the data
 
-print ("###################### Data Loaded ######################")
+print("###################### Data Loaded ######################")
 print("Data Shape:   ")
-print("X train: ",X_train.shape)
-print("Y train: ",y_train.shape)
-print("X test: ",X_test.shape)
-print("Y test: ",y_test.shape)
-
+print("X train: ", X_train.shape)
+print("Y train: ", y_train.shape)
+print("X test: ", X_test.shape)
+print("Y test: ", y_test.shape)
 
 y_train_labels = []
 
@@ -52,7 +52,6 @@ for label in y_train[1]:
         continue
 
     y_train_labels.append(int(label))
-
 
 pd_y_train_labels = pd.DataFrame(y_train_labels)
 pd_y_train_images = y_train[0]
@@ -68,8 +67,6 @@ for label in y_test[1]:
 pd_y_test_labels = pd.DataFrame(y_test_labels)
 pd_y_test_images = y_test[0]
 
-
-
 # Convert Pandas to Numpy
 y_train_labels = pd_y_train_labels.to_numpy().flatten()
 y_train_images = pd_y_train_images.to_numpy()
@@ -78,63 +75,77 @@ y_test_labels = pd_y_test_labels.to_numpy()
 y_test_images = pd_y_test_images.to_numpy()
 
 
+def train():
+    # Model Learning
+    Input1 = {}
 
-# Model Learning
-Input1 = {}
+    Input1['Images'] = y_train_images
+    Input1['Features'] = X_train
+    Input1['Labels'] = y_train_labels
 
-Input1['Images'] = y_train_images
-Input1['Features'] = X_train
-Input1['Labels'] = y_train_labels
+    Mode1 = 'Learning'
 
-Mode1 = 'Learning'
+    return xDNN(Input1,Mode1)
 
-start = time.time()
-Output1 = xDNN(Input1,Mode1)
 
-end = time.time()
-
-print ("###################### Model Trained ####################")
-
-print("Time: ",round(end - start,2), "seconds")
 ###############################################################################
 
 # Load the files, including features, images and labels for the validation mode
 
-Input2 = {}
-Input2['xDNNParms'] = Output1['xDNNParms']
-Input2['Images'] = y_test_images 
-Input2['Features'] = X_test
-Input2['Labels'] = y_test_labels 
+def validate(training_results):
+    Input2 = {}
+    Input2['xDNNParms'] = training_results['xDNNParms']
+    Input2['Images'] = y_test_images
+    Input2['Features'] = X_test
+    Input2['Labels'] = y_test_labels
+    Mode2 = 'Validation'
+    return xDNN(Input2, Mode2)
 
 
+def classify(training_results, features):
+    classification_input = {}
+    classification_input['xDNNParms'] = training_results['xDNNParms']
+    classification_input['Features'] = features
+    classification_mode = 'classify'
 
-startValidation = time.time()
-Mode2 = 'Validation' 
-Output2 = xDNN(Input2,Mode2)
-endValidation = time.time()
-
-print ("###################### Results ##########################")
+    return xDNN(classification_input, classification_mode)
 
 
-# Elapsed Time
-print("Time: ", round(endValidation - startValidation,2), "seconds")
-# accuracy: (tp + tn) / (p + n)
-accuracy = accuracy_score(y_test_labels , Output2['EstLabs'])
-print('Accuracy: %f' % accuracy)
-# precision tp / (tp + fp)
-precision = precision_score(y_test_labels , Output2['EstLabs'], average='micro')
-print('Precision: %f' % precision)
-# recall: tp / (tp + fn)
-recall = recall_score(y_test_labels , Output2['EstLabs'],average='micro')
-print('Recall: %f' % recall)
-# f1: 2 tp / (2 tp + fp + fn)
-f1 = f1_score(y_test_labels , Output2['EstLabs'], average='micro')
-print('F1 score: %f' % f1)
-# kappa
-kappa = cohen_kappa_score(y_test_labels , Output2['EstLabs'])
-print('Cohens kappa: %f' % kappa)
+if __name__ == "__main__":
+    start = time.time()
+    Output1 = train()
 
-# confusion matrix
-matrix = confusion_matrix(y_test_labels , Output2['EstLabs'])
-print("Confusion Matrix: ",matrix)
+    end = time.time()
+
+    print("###################### Model Trained ####################")
+
+    print("Time: ", round(end - start, 2), "seconds")
+
+    startValidation = time.time()
+    Output2 = validate(Output1)
+    endValidation = time.time()
+
+    print("###################### Results ##########################")
+
+    # Elapsed Time
+    print("Time: ", round(endValidation - startValidation, 2), "seconds")
+    # accuracy: (tp + tn) / (p + n)
+    accuracy = accuracy_score(y_test_labels, Output2['EstLabs'])
+    print('Accuracy: %f' % accuracy)
+    # precision tp / (tp + fp)
+    precision = precision_score(y_test_labels, Output2['EstLabs'], average='micro')
+    print('Precision: %f' % precision)
+    # recall: tp / (tp + fn)
+    recall = recall_score(y_test_labels, Output2['EstLabs'], average='micro')
+    print('Recall: %f' % recall)
+    # f1: 2 tp / (2 tp + fp + fn)
+    f1 = f1_score(y_test_labels, Output2['EstLabs'], average='micro')
+    print('F1 score: %f' % f1)
+    # kappa
+    kappa = cohen_kappa_score(y_test_labels, Output2['EstLabs'])
+    print('Cohens kappa: %f' % kappa)
+
+    # confusion matrix
+    matrix = confusion_matrix(y_test_labels, Output2['EstLabs'])
+    print("Confusion Matrix: ", matrix)
 
