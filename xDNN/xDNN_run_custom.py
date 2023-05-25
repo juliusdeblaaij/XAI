a3e0386d2c3d50325a1236fd3ecc5203116328e6
Julius de Blaaij <julius.de.blaaij@gmail.com>
2023-05-25 09:31:31 +0200

    Re-wrote custom xDNN to be seperated into funcrions for training and validation

--- xDNN/xDNN_run_custom.py
+++ xDNN/xDNN_run_custom.py
@@ -17,7 +17,7 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import cohen_kappa_score
 from sklearn.metrics import roc_auc_score
-from sklearn.metrics import confusion_matrix
+from nltk.metrics import ConfusionMatrix
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 import time
@@ -30,19 +30,18 @@ X_test_file_path = r'../data_df_X_test.csv'
 y_test_file_path = r'../data_df_y_test.csv'
 
 X_train = genfromtxt(X_train_file_path, delimiter=',')
-y_train = pd.read_csv(y_train_file_path, delimiter=',',header=None)
+y_train = pd.read_csv(y_train_file_path, delimiter=',', header=None)
 X_test = genfromtxt(X_test_file_path, delimiter=',')
-y_test = pd.read_csv(y_test_file_path, delimiter=',',header=None)
+y_test = pd.read_csv(y_test_file_path, delimiter=',', header=None)
 
 # Print the shape of the data
 
-print ("###################### Data Loaded ######################")
+print("###################### Data Loaded ######################")
 print("Data Shape:   ")
-print("X train: ",X_train.shape)
-print("Y train: ",y_train.shape)
-print("X test: ",X_test.shape)
-print("Y test: ",y_test.shape)
-
+print("X train: ", X_train.shape)
+print("Y train: ", y_train.shape)
+print("X test: ", X_test.shape)
+print("Y test: ", y_test.shape)
 
 y_train_labels = []
 
@@ -52,7 +51,6 @@ for label in y_train[1]:
         continue
 
     y_train_labels.append(int(label))
-
 
 pd_y_train_labels = pd.DataFrame(y_train_labels)
 pd_y_train_images = y_train[0]
@@ -68,8 +66,6 @@ for label in y_test[1]:
 pd_y_test_labels = pd.DataFrame(y_test_labels)
 pd_y_test_images = y_test[0]
 
-
-
 # Convert Pandas to Numpy
 y_train_labels = pd_y_train_labels.to_numpy().flatten()
 y_train_images = pd_y_train_images.to_numpy()
@@ -78,63 +74,88 @@ y_test_labels = pd_y_test_labels.to_numpy()
 y_test_images = pd_y_test_images.to_numpy()
 
 
+def train():
+    data = {'Images': y_train_images, 'Features': X_train, 'Labels': y_train_labels}
 
-# Model Learning
-Input1 = {}
+    mode = 'Learning'
 
-Input1['Images'] = y_train_images
-Input1['Features'] = X_train
-Input1['Labels'] = y_train_labels
-
-Mode1 = 'Learning'
-
-start = time.time()
-Output1 = xDNN(Input1,Mode1)
-
-end = time.time()
-
-print ("###################### Model Trained ####################")
-
-print("Time: ",round(end - start,2), "seconds")
-###############################################################################
-
-# Load the files, including features, images and labels for the validation mode
-
-Input2 = {}
-Input2['xDNNParms'] = Output1['xDNNParms']
-Input2['Images'] = y_test_images 
-Input2['Features'] = X_test
-Input2['Labels'] = y_test_labels 
+    return xDNN(data, mode)
 
 
+def validate(training_results):
+    data = {'xDNNParms': training_results['xDNNParms'], 'Images': y_test_images, 'Features': X_test,
+             'Labels': y_test_labels}
 
-startValidation = time.time()
-Mode2 = 'Validation' 
-Output2 = xDNN(Input2,Mode2)
-endValidation = time.time()
-
-print ("###################### Results ##########################")
+    mode = 'Validation'
+    return xDNN(data, mode)
 
 
-# Elapsed Time
-print("Time: ", round(endValidation - startValidation,2), "seconds")
-# accuracy: (tp + tn) / (p + n)
-accuracy = accuracy_score(y_test_labels , Output2['EstLabs'])
-print('Accuracy: %f' % accuracy)
-# precision tp / (tp + fp)
-precision = precision_score(y_test_labels , Output2['EstLabs'], average='micro')
-print('Precision: %f' % precision)
-# recall: tp / (tp + fn)
-recall = recall_score(y_test_labels , Output2['EstLabs'],average='micro')
-print('Recall: %f' % recall)
-# f1: 2 tp / (2 tp + fp + fn)
-f1 = f1_score(y_test_labels , Output2['EstLabs'], average='micro')
-print('F1 score: %f' % f1)
-# kappa
-kappa = cohen_kappa_score(y_test_labels , Output2['EstLabs'])
-print('Cohens kappa: %f' % kappa)
+if __name__ == "__main__":
+    startTraining = time.time()
+    training_results = train()
+    endTraining = time.time()
 
-# confusion matrix
-matrix = confusion_matrix(y_test_labels , Output2['EstLabs'])
-print("Confusion Matrix: ",matrix)
+    print("Model trained in: ", round(endTraining - startTraining, 2), "seconds")
 
+    startValidation = time.time()
+    validation_results = validate(training_results)
+    endValidation = time.time()
+
+    # Elapsed Time
+    print("Time: ", round(endValidation - startValidation, 2), "seconds")
+    # accuracy: (tp + tn) / (p + n)
+    accuracy = accuracy_score(y_test_labels, validation_results['EstLabs'])
+    print('Accuracy: %f' % accuracy)
+    # precision tp / (tp + fp)
+    precision = precision_score(y_test_labels, validation_results['EstLabs'], average='micro')
+    print('Precision: %f' % precision)
+    # recall: tp / (tp + fn)
+    recall = recall_score(y_test_labels, validation_results['EstLabs'], average='micro')
+    print('Recall: %f' % recall)
+    # f1: 2 tp / (2 tp + fp + fn)
+    f1 = f1_score(y_test_labels, validation_results['EstLabs'], average='micro')
+    print('F1 score: %f' % f1)
+    # kappa
+    kappa = cohen_kappa_score(y_test_labels, validation_results['EstLabs'])
+    print('Cohens kappa: %f' % kappa)
+
+    y_test_labels = list(y_test_labels.flatten())
+    estimated_labels = validation_results['EstLabs'].flatten().astype(int)
+
+    y_test_labels_reclassified = []
+
+    for label in y_test_labels:
+        if label == 0:
+            y_test_labels_reclassified.append(0)
+        elif label == 1:
+            y_test_labels_reclassified.append(1)
+        elif label == 2:
+            y_test_labels_reclassified.append(2)
+        elif label == 3:
+            y_test_labels_reclassified.append(3)
+        elif label == 4:
+            y_test_labels_reclassified.append(5)
+        elif label == 5:
+            y_test_labels_reclassified.append(8)
+
+    estimated_labels_reclassified = []
+
+    for label in estimated_labels:
+        if label == 0:
+            estimated_labels_reclassified.append(0)
+        elif label == 1:
+            estimated_labels_reclassified.append(1)
+        elif label == 2:
+            estimated_labels_reclassified.append(2)
+        elif label == 3:
+            estimated_labels_reclassified.append(3)
+        elif label == 4:
+            estimated_labels_reclassified.append(5)
+        elif label == 5:
+            estimated_labels_reclassified.append(8)
+
+
+    # confusion matrix
+    matrix = ConfusionMatrix(y_test_labels_reclassified, estimated_labels_reclassified)
+    print("Confusion Matrix:")
+    print(matrix)
